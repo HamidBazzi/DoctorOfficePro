4d1b750557ebf0d4094916e3adce98cdee04f7df
Hamid Bazzi <bazzi.hamid@gmail.com>
2025-04-20 23:33:27 +0330

    Create interface and repository for Patient

---
+++ Repository/IRepository/IPatientRepository.cs
@@ -0,0 +1,22 @@
+﻿using DoctorOfficePro.Data;
+
+namespace DoctorOfficePro.Repository.IRepository
+{
+    public interface IPatientRepository
+    {
+        // Create Patient
+        public Patient Create(Patient obj);
+
+        // Update Patient
+        public Patient Update(Patient obj);
+
+        // Delete Patient
+        public bool Delete(int id);
+
+        // Get Patient by id
+        public Patient Get(int id);
+
+        // Get All Patient
+        public IEnumerable<Patient> GetAll();
+    }
+}
---
+++ Repository/PatientRepository.cs
@@ -0,0 +1,73 @@
+﻿using DoctorOfficePro.Data;
+using DoctorOfficePro.Repository.IRepository;
+
+namespace DoctorOfficePro.Repository
+{
+    public class PatientRepository : IPatientRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public PatientRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public Patient Create(Patient obj)
+        {
+            _db.Patients.Add(obj);
+            _db.SaveChanges();
+            return obj;
+        }
+
+        public bool Delete(int id)
+        {
+            var obj = _db.Patients.FirstOrDefault(u => u.Id == id);
+            if(obj != null)
+            {
+                _db.Patients.Remove(obj);
+                return _db.SaveChanges() > 0;
+            }
+            return false;
+        }
+
+        public Patient Get(int id)
+        {
+            var obj = _db.Patients.FirstOrDefault(u => u.Id == id);
+            if(obj == null)
+            {
+                return new Patient();
+            }
+            return obj;
+        }
+
+        public IEnumerable<Patient> GetAll()
+        {
+            return _db.Patients.ToList();
+        }
+
+        public Patient Update(Patient obj)
+        {
+            var objFromDb = _db.Patients.FirstOrDefault(u => u.Id == obj.Id);
+            if(objFromDb is not null)
+            {
+                objFromDb.FirstName = obj.FirstName;
+                objFromDb.LastName = obj.LastName;
+                objFromDb.NationalCode = obj.NationalCode;
+                objFromDb.DateOfBirth = obj.DateOfBirth;
+                objFromDb.Gender = obj.Gender;
+                objFromDb.PhoneNumber = obj.PhoneNumber;
+                objFromDb.Email = obj.Email;
+                objFromDb.Address = obj.Address;
+                objFromDb.InsuranceType = obj.InsuranceType;
+                objFromDb.InsuranceNumber = obj.InsuranceNumber;
+                objFromDb.BloodType = obj.BloodType;
+                objFromDb.Allergies = obj.Allergies;
+                objFromDb.ChronicDiseases = obj.ChronicDiseases;
+                objFromDb.CreatedAt = obj.CreatedAt;
+
+                _db.Patients.Update(objFromDb);
+                _db.SaveChanges();
+                return objFromDb;
+            }
+            return obj;
+        }
+    }
+}
